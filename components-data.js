3e27b737f84f325245455524444bd2d69dba71ee
Ezequiel Gioia <eze1981@gmail.com>
2016-04-27 00:04:42 -0400

    add util.js, some test cases and json metadata extraction

--- components-data.js
+++ components-data.js
@@ -1,4 +1,4 @@
-cf-components = {
+{
   "components": [
     {
       "DISPLAYNAME": "Application",
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var fs = require('fs')
 var analyzer = require("./lib/analyzer.js")
 var cytoscape = require("./lib/cytoscape-format.js")
 var vis = require("./lib/visjs-format.js")
+var docEngine = require("./lib/doc-engine.js")
 
 var save = (data, fileName) => {
   return new Promise((resolve, reject) => {
@@ -23,12 +24,33 @@ var save = (data, fileName) => {
 }
 
 //TODO: validate app parameters
-var directoryToAnalyze = process.argv[2]
-console.log(directoryToAnalyze)
+//var directoryToAnalyze = process.argv[2]
+//console.log(directoryToAnalyze)
 
+/*
 analyzer
   .analyze(directoryToAnalyze)
     .then(result => vis.format(result))
     //.then(result => console.log(JSON.stringify(result)))
     .then(visData => save(visData, 'dataset.js'))
     //.then(() => console.log('exit'))
+
+*/
+
+/*
+1. get json metadata, http GET
+2. generate MD
+  2.1 create 1 file per component in metadata
+3. generate HTML
+*/
+/*
+var docEngine = null
+docEngine.getMetadata()
+  .then(metadata => generateMD(metadata))
+  .then(mdFiles => generateHTML(mdFiles))
+*/
+
+docEngine.getMetadata('./components-data.js')
+  .then(metadata => docEngine.generateMD(metadata))
+  //.then(metadata => console.log(metadata))
+  .catch(err => console.log(err))
