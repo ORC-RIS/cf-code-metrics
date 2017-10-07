cf758a00101d81f7158416ad2b15b7951f1c0ae2
Ezequiel Gioia <eze1981@gmail.com>
2017-10-06 21:33:59 -0400

    Replaced Vuetify with mdl

--- doc-browser/src/App.vue
+++ doc-browser/src/App.vue
@@ -1,4 +1,26 @@
 <template>
+
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Documents</span>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Documents</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a picture</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
+
+<!--
   <div>
     <v-app toolbar footer>
         <v-navigation-drawer
@@ -41,9 +63,12 @@
 
     </v-app>
   </div>
+  -->
 </template>
 
 <script>
+require('material-design-lite')
+
 export default {
   name: 'app',
   data () {
@@ -52,11 +77,20 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       drawer: true
     }
+  },
+  methods: {
+    hideMenu: function () {
+      document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+      document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+    }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+
 #app {
 
 }
--- doc-browser/src/components/Dashboard.vue
+++ doc-browser/src/components/Dashboard.vue
@@ -1,4 +1,21 @@
 <template>
+<div class="mdl-grid">
+
+  <div v-for="item in projects" :key="item.project" class="mdl-cell--4-col mdl-cell mdl-card mdl-shadow--2dp">
+    <div class="mdl-card__title">
+      <h2 class="mdl-card__title-text">{{item.project}}</h2>
+    </div>
+    <div class="mdl-card__supporting-text">content</div>
+    <div class="mdl-card__actions mdl-card--border">
+      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+        Get Started
+      </a>
+      
+    </div>
+  </div>
+
+</div>
+<!--
   <v-layout>
 
       <v-layout row wrap>
@@ -30,63 +47,8 @@
       </v-layout>
 
   </v-layout>
-<!--
-  <v-container fluid grid-list-md text-xs-center>
-
-    <h1>Items: {{projects.length}}</h1>
-
-    <v-card>
-      <v-card-title>
-        TITLE
-      </v-card-title>
-      <v-card-text>
-        eze
-      </v-card-text>
-      <v-card-actions class="white">
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>favorite</v-icon>
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    
-    <p></p>
-
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card dark class="primary">
-          <v-card-text class="px-0">12</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 v-for="i in 2" :key="i">
-        <v-card dark class="secondary">
-          <v-card-text class="px-0">6</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs4 v-for="i in 3" :key="i">
-        <v-card dark class="primary">
-          <v-card-text class="px-0">4</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs3 v-for="i in 4" :key="i">
-        <v-card dark class="secondary">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs2 v-for="i in 6" :key="i">
-        <v-card dark class="primary">
-          <v-card-text class="px-0">2</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs1 v-for="i in 12" :key="i">
-        <v-card dark class="secondary">
-          <v-card-text class="px-0">1</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
-
   -->
+
 </template>
 
 <script>
