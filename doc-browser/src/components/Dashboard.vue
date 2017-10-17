49fd1459795a88423e4777459b3f02884dec8c6d
Ezequiel Gioia <eze1981@gmail.com>
2017-10-16 21:05:33 -0400

    Improving dashboard layout

--- doc-browser/src/components/Dashboard.vue
+++ doc-browser/src/components/Dashboard.vue
@@ -1,17 +1,5 @@
 <template>
-
 <div>
-  <!--
-  <div class="tabs">
-    <ul>
-      <li>Datasources</li>
-      <li>Queries</li>
-      <li>Stored procedures</li>
-      <li>Components</li>
-      <li>Includes</li>
-    </ul>
-  </div>
-  -->
 
   <div class="columns is-multiline">
     <div class="column is-4" v-for="item in projects" :key="item.project"
@@ -21,57 +9,32 @@
           <div class="media">
             <div class="media-content">
               <p class="title is-4">{{item.project}}</p>
-              <p class="subtitle is-6">description</p>
+              <!-- <p class="subtitle is-6">description</p> -->
             </div>
           </div>
 
-          <div class="content">
-            <ul>
-              <li class="is-fluid">
-                <div class="is-size-4 metric-value">{{item.datasources}}</div>
-                <div class="is-size-6">datasources</div>
-              </li>
-              <li>
-                <span class="is-size-4 metric-value">{{item.components}}</span>
-                <span class="is-size-6">components</span>
-              </li>
-              <li>
-                <span class="is-size-4 metric-value">{{item.sps}}</span>
-                <span class="is-size-6">stores procedures</span>
-              </li>
-              <li>
-                <span class="is-size-4 metric-value">{{item.queries}}</span>
-                <span class="is-size-6">queries</span>
-              </li>
-              <li>
-                <span class="is-size-4 metric-value">{{item.includes}}</span>
-                <span class="is-size-6">includes</span>
-              </li>
-            </ul>
+          <div class="columns">
+            <div class="column is-2 metric-value">
+                <div>{{item.datasources}}</div>
+                <div>{{item.components}}</div>
+                <div>{{item.sps}}</div>
+                <div>{{item.queries}}</div>
+                <div>{{item.includes}}</div>
+            </div>
+            
+            <div class="column">
+              <div class="headingss">datasources</div>
+              <div class="headingss">components</div>
+              <div>stores procedures</div>
+              <div>queries</div>
+              <div>includes</div>
+            </div>
+          </div>
+
           </div>
         </div>
     </div>
   </div>
-
-</div>
-
-
-
-<!--
-  <div v-for="item in projects" :key="item.projectId" 
-    v-on:click="viewProject(item.projectId)"
-    class="mdl-cell--4-col mdl-cell mdl-card mdl-shadow--2dp">
-    <div class="mdl-card__title">
-      <h2 class="mdl-card__title-text">{{item.project}}</h2>
-    </div>
-    <div class="mdl-card__supporting-text">content</div>
-    <div class="mdl-card__actions mdl-card--border">
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-        Get Started
-      </a>      
-    </div>
-  </div>
--->
 
 </div>
 </template>
@@ -115,7 +78,7 @@ li {
 }
 
 .metric-value {
-  width: 40px;
+  text-align: right;
 }
 
 </style>
